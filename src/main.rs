642237a584232526136543a381e3745334ee8d7a
KristenBelkadiEpsi <kristen.belkadi.etudiant@gmail.com>
2024-06-25 17:02:12 +0200

    ajout de la config sonar + tests unitaires en cours

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,6 @@ use actix_web::{web, App, HttpResponse, HttpServer, Responder};
 use chrono::DateTime;
 use dotenv::dotenv;
 use orders::model;
-/* use postgres::{Client, Error, NoTls}; */
 use serde::Deserialize;
 use serde_json::json;
 use std::str::FromStr;
@@ -29,7 +28,7 @@ async fn get_orders(pagination: web::Query<Pagination>) -> impl Responder {
     );
 
     let total_count: i64 = client
-        .query_one("SELECT COUNT(*) AS total_count FROM orders", &[])
+        .query_one(r#"SELECT COUNT(*) AS total_count FROM "orders""#, &[])
         .await
         .unwrap()
         .get("total_count");
@@ -37,7 +36,7 @@ async fn get_orders(pagination: web::Query<Pagination>) -> impl Responder {
     for row in client.query(query.as_str(), &[]).await.unwrap() {
         let order = OrderModel {
             id: Uuid::from_str(row.get("id")).unwrap(),
-            created_at: DateTime::parse_from_rfc3339(row.get("date_created"))
+            created_at: DateTime::parse_from_rfc3339(row.get("created_at"))
                 .unwrap()
                 .to_utc(),
             customer_id: Uuid::from_str(row.get("customer_id")).unwrap(),
@@ -48,18 +47,18 @@ async fn get_orders(pagination: web::Query<Pagination>) -> impl Responder {
     HttpResponse::Ok().json(json!({"value":orders,"total_count":total_count}))
 }
 
-async fn get_order_by_id(order_id: web::Path<i32>) -> impl Responder {
+async fn get_order_by_id(order_id: web::Path<Uuid>) -> impl Responder {
     let client = get_connection().await.unwrap();
 
-    let query = format!("SELECT * FROM orders WHERE id = {}", order_id);
-    let row = client.query_one(query.as_str(), &[]).await.unwrap();
+    let row = client
+        .query_one(r#"SELECT * FROM "orders" WHERE id = $1"#, &[&*order_id])
+        .await
+        .unwrap();
 
     let order = OrderModel {
-        id: Uuid::from_str(row.get("id")).unwrap(),
-        created_at: DateTime::parse_from_rfc3339(row.get("date_created"))
-            .unwrap()
-            .to_utc(),
-        customer_id: Uuid::from_str(row.get("customer_id")).unwrap(),
+        id: row.get("id"),
+        created_at: row.get("created_at"),
+        customer_id: row.get("customer_id"),
     };
 
     HttpResponse::Ok().json(order)
@@ -72,11 +71,7 @@ async fn create_order(new_order: web::Json<CreateOrderDTO>) -> impl Responder {
     let result = client
         .query_one(
             query,
-            &[
-                &new_uuid.to_string(),
-                &new_order.created_at.to_rfc3339(),
-                &new_order.customer_id.to_string(),
-            ],
+            &[&new_uuid, &new_order.created_at, &new_order.customer_id],
         )
         .await
         .unwrap();
@@ -88,7 +83,7 @@ async fn create_order(new_order: web::Json<CreateOrderDTO>) -> impl Responder {
 }
 
 async fn update_order(
-    order_id: web::Path<i32>,
+    order_id: web::Path<Uuid>,
     updated_order: web::Json<UpdateOrderDTO>,
 ) -> impl Responder {
     let client = get_connection().await.unwrap();
@@ -98,9 +93,9 @@ async fn update_order(
         .execute(
             query,
             &[
-                &updated_order.created_at.to_rfc3339(),
-                &updated_order.customer_id.to_string(),
-                &order_id.to_string(),
+                &updated_order.created_at,
+                &updated_order.customer_id,
+                &*order_id,
             ],
         )
         .await
@@ -109,7 +104,7 @@ async fn update_order(
     HttpResponse::Ok().body(format!("Updated order with id: {}", order_id))
 }
 
-async fn delete_order(order_id: web::Path<i32>) -> impl Responder {
+async fn delete_order(order_id: web::Path<Uuid>) -> impl Responder {
     let client = get_connection().await.unwrap();
 
     let query = "DELETE FROM orders WHERE id = $1";
@@ -127,7 +122,7 @@ pub async fn init_database() -> Result<(), Error> {
         .execute(
             r#"CREATE TABLE IF NOT EXISTS orders (
     id UUID PRIMARY KEY,
-    created_at TIMESTAMP,
+    created_at TIMESTAMPTZ,
     customer_id UUID
     )"#,
             &[],
@@ -146,7 +141,6 @@ pub async fn get_connection() -> Result<Client, Error> {
         }
     });
     Ok(client)
-    /*  Client::connect(&url, NoTls) */
 }
 
 #[actix_web::main]
@@ -165,7 +159,99 @@ async fn main() -> std::io::Result<()> {
             .route("/orders/{id}", web::put().to(update_order))
             .route("/orders/{id}", web::delete().to(delete_order))
     })
-    .bind("127.0.0.1:8080")?
+    .bind(("localhost", 8080))?
     .run()
     .await
+}
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use actix_web::{test, web, App};
+    use chrono::Utc;
+    use uuid::Uuid;
+
+    async fn setup_test_app() -> impl actix_web::dev::Service<
+        actix_http::Request,
+        Response = actix_web::dev::ServiceResponse,
+        Error = actix_web::Error,
+    > {
+        test::init_service(
+            App::new()
+                .route("/orders", web::get().to(get_orders))
+                .route("/orders/{id}", web::get().to(get_order_by_id))
+                .route("/orders", web::post().to(create_order))
+                .route("/orders/{id}", web::put().to(update_order))
+                .route("/orders/{id}", web::delete().to(delete_order)),
+        )
+        .await
+    }
+
+    #[actix_rt::test]
+    async fn test_get_orders() {
+        let app = setup_test_app().await;
+        let req = test::TestRequest::get()
+            .uri("/orders?page=1&per_page=10")
+            .to_request();
+        let resp = test::call_service(&app, req).await;
+        assert!(resp.status().is_success());
+    }
+
+    #[actix_rt::test]
+    async fn test_get_order_by_id() {
+        let app = setup_test_app().await;
+        let req = test::TestRequest::get().uri("/orders/1").to_request();
+        let resp = test::call_service(&app, req).await;
+        println!("{:?}", resp.status());
+        assert!(resp.status().is_success());
+    }
+
+    #[actix_rt::test]
+    async fn test_create_order() {
+        let app = setup_test_app().await;
+        let new_order = CreateOrderDTO {
+            created_at: Utc::now(),
+            customer_id: Uuid::new_v4(),
+        };
+        let req = test::TestRequest::post()
+            .uri("/orders")
+            .set_json(&new_order)
+            .to_request();
+        let resp = test::call_service(&app, req).await;
+        assert!(resp.status().is_success());
+    }
+
+    #[actix_rt::test]
+    async fn test_update_order() {
+        let app = setup_test_app().await;
+        let updated_order = UpdateOrderDTO {
+            created_at: Utc::now(),
+            customer_id: Uuid::new_v4(),
+        };
+        let req = test::TestRequest::put()
+            .uri("/orders/1")
+            .set_json(&updated_order)
+            .to_request();
+        let resp = test::call_service(&app, req).await;
+        assert!(resp.status().is_success());
+    }
+
+    #[actix_rt::test]
+    async fn test_delete_order() {
+        let app = setup_test_app().await;
+        let req = test::TestRequest::delete().uri("/orders/1").to_request();
+        let resp = test::call_service(&app, req).await;
+        assert!(resp.status().is_success());
+    }
+
+    #[tokio::test]
+    async fn test_init_database() {
+        let result = init_database().await;
+        assert!(result.is_ok());
+    }
+
+    #[tokio::test]
+    async fn test_get_connection() {
+        let result = get_connection().await;
+        assert!(result.is_ok());
+    }
 }
